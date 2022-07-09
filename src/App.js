aa10199c020af882efec531e954334f58983c14d
odilbek000 <odilbekxaker@gmail.com>
2022-07-09 11:26:45 +0500

    tour app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import '../src/Assets/sass/main.scss';
+
+// components
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <>
+      <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+// images
+import Logo from "../../Assets/images/logo.png";
+
+export default function Header(){
+    return(
+        <section>
+            <div className="container">
+                <header className="header">
+                    <img src={Logo} width={112} height={47} alt="logo"/>
+                    <ul className="header__list">
+                        <li className="header__item"><a className="header__item--link active" href="/">Bosh sahifa</a></li>
+                        <li className="header__item"><a className="header__item--link" href="/">Shaharlar</a></li>
+                        <li className="header__item"><a className="header__item--link" href="/">Muhim malumotlar</a></li>
+                        <li className="header__item"><a className="header__item--link" href="/">Kontakt</a></li>
+                        <li className="header__item"><a className="header__item--link" href="/">Biz haqimizda</a></li>
+                    </ul>
+                    <select className="header__select" id="selected">
+                        <option className="header__option" value="en">English</option>
+                        <option className="header__option" value="en">Russia</option>
+                        <option className="header__option" value="en">Uzbek</option>
+                    </select>
+                    <div>
+                        <input className="header__search--input" type="text" placeholder="Search"/>
+                    </div>
+                </header>
+            </div>
+        </section>
+    )
+}
