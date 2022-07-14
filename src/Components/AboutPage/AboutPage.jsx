7941fd62c7a1792bd70373ead8ba2ec89e5f2dad
Safarov Odilbek Web developer <91378049+odilbekjon@users.noreply.github.com>
2022-07-14 14:20:05 +0500

    update change
    add change

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -84,9 +84,9 @@ export default function AboutPage(){
                     <h1 className="about__head">Loyiha haqida</h1>
                     <div className="about__box">
                         <ul className="about__box--list">
-                            <li className="about__box--item"><a className="about__box--link green" href="">Loyiha haqida</a></li>
-                            <li className="about__box--item"><a className="about__box--link" href="">Uning maqsadi</a></li>
-                            <li className="about__box--item"><a className="about__box--link" href="">Lorem ipsum</a></li>
+                            <li className="about__box--item"><a className="about__box--link green" href="#">Loyiha haqida</a></li>
+                            <li className="about__box--item"><a className="about__box--link" href="#">Uning maqsadi</a></li>
+                            <li className="about__box--item"><a className="about__box--link" href="#">Lorem ipsum</a></li>
                         </ul>
                         <p className="about__box--text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                     </div>
