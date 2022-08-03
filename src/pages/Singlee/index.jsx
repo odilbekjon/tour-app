12d0e202f2bf9e5211f1df8276140cde733d1ad1
odilbek000 <odilbekxaker@gmail.com>
2022-08-03 17:15:53 +0500

    change xarita

--- src/pages/Singlee/index.jsx
+++ src/pages/Singlee/index.jsx
@@ -1,12 +1,19 @@
 import Single1 from "../../Components/SinglePage/Single1/Single";
 import { GaleryCarousel } from "../../Components/Carousels/GaleryCarousel";
 import { MainCarousel_one } from "../../Components/Carousels/MainCarousel_one";
+import "./single.scss";
+
+// images
+import Map from "../../Assets/images/map.png";
 
 export const Single = () => {
     return (
         <>
             <Single1 />
             <GaleryCarousel title={'Fotogaleriya'} />
+            <div className="container">
+                <img className="map__images" src={Map} alt="image" />
+            </div>
             <MainCarousel_one title={'O’xshash ziyoratgohlar'} />
         </>
     )
